508042f32920c823af229e52b4f0090d498336a1
MuntoniFrancesca <francesca.muntoni@live.com>
2023-11-02 12:26:27 -0500

    Working on TASK2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    //Employer should have a field called location
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String location;
+    //
+
+    //No args constructor:
+    public Employer () {}
+    //
+
+    //Add public accessors methods for Employer
+    public String getLocation() {
+        return location;
+    }
+    //
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,18 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    //add a description field
+    private String description;
+
+    //no args constructor
+    public Skill() {}
+
+    //public access methods
+    public String getDescription() {
+        return description;
+    }
 }
